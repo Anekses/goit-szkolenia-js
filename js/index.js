7c6c8f792114b4a5a97c8c8112925dd5cc478b1e
AdrianRydzynski <adrian.rydzynski@gmail.com>
2023-07-17 21:02:24 +0200

    1 class

---
+++ js/index.js
@@ -0,0 +1,56 @@
+// "use strict";
+
+// const a = 5;
+// const b = 0;
+// const result = a / b;
+// console.log(result)
+
+// const TOTAL_NUMBER_OF_PHOTOS = 5;
+
+// const firstname = 'Tomasz';
+// const age = 38;
+// const isOldEnoughToDrive = true;
+// const residence = null;
+
+// const a;
+
+// let userAge = 32
+// let userName;
+// let userAge = 44
+// userAge = 44
+
+// console.log(userAge, userName, residence)
+
+// const age = 'abba'
+// const a = 0;
+// const password = 0;
+// const c = 0;
+// const totalNumberOfPhotos = 0;
+// const c2 = 0;
+// const const = 3;
+// let if = 5;
+// const true = false;
+
+// console.log({firstname, age, isOldEnoughToDrive})
+
+
+// const message = 'Łukasz';
+// console.log("My name is: ", message)
+// alert(message)
+
+// const isComing = confirm('Please confirm your reservation')
+// console.log(isComing)
+
+// const hotelName = prompt('Please enter hotel name')
+// console.log(hotelName)
+
+// const first = 5;
+// const second = "5";
+// const notANumber = "5.3px12"
+
+const message = "Mango";
+const secondMessage = "is";
+const thirdMessage = "happy";
+
+const someString = `This is my message ${message}`
+const someOtherString = "This is my message" + " " + message
