9e34c40e5df057bc8cf710c3596fcd648e53a7a8
AdrianRydzynski <adrian.rydzynski@gmail.com>
2023-08-02 20:38:45 +0200

    6 class

--- js/index.js
+++ js/index.js
@@ -1,230 +1,215 @@
-// const title = 'Harry Potter'
-// const author = 'J.K. Rowling'
-// const isPublic = true
-// const rating = 9.2
+// // const someArrayWithNumbers = [1, 2, 13, 4, -89, 4, 46, 27]
+// // // console.log(someArrayWithNumbers)
+// // // console.log(...someArrayWithNumbers)
+
+// // // console.log(someArrayWithNumbers)
+
+// // // console.log(Math.max(...someArrayWithNumbers))
+
+// // // Math.max(...[1, 2, 13, 4]) -> Math.max(1, 2, 13, 4)
+
+// // const copyOfArray = [...someArrayWithNumbers]
+// // // const copyOfArray2 = someArrayWithNumbers
+
+// // const otherArrayAsWell = [6, 76, 8]
+// // const secondCopyOfArray = [...someArrayWithNumbers, ...otherArrayAsWell]
+
+// // const firstObj = { name: 'Bartek', age: 25}
+// // const secondObj = { whatDoOnWednesdayEvening: 'learning'}
+
+// // console.log(firstObj, secondObj)
+
+// // const thirdObj = { age: 30, ...firstObj, ...secondObj, isHePlayingGuitar: false, }
+
+// // console.log(thirdObj)
+
+// // const baseRequest = {
+// //   nationality: 'Poland'
+// // }
+
+// // const requestToBackend = {
+// //   ...baseRequest,
+// //   nationality: 'Czech'
+// // }
+
+// // const obj123 = {
+// //   a: 1,
+// //   b: 2,
+// //   c: 3
+// // }
+
+// // console.log(obj123)
+// // console.log({...obj123})
+
+// // function multiply(isUpdate, ...args) {
+// //   isUpdate ? invokeFirstFunction(args) : invokeOfSomeAwesomeFunction(args)
+// // }
+
+// // multiply(true, 1, 2)
+// // multiply(false, 1, 2, 3)
+// // multiply(1, 2, 3, 4)
 
 // const book = {
-//   title: 'Harry Potter',
+//   title: 'Harry Potter 6',
 //   author: 'J.K. Rowling',
+//   genres: ['adventure', 'somethingAwesome'],
 //   isPublic: true,
-//   rating: 9.2,
-//   genres: ['awesome', 'good to read'],
-//   publishingInfo: {
-//     year: 1994,
-//     numberOfPages: 316,
-//     isAwesome: true
-//   }
-//   // someObject: {
-//   //   a: 1,
-//   //   b: "2",
-//   //   c: false,
-//   //   d: {
-//   //     e: 2,
-//   //     f: 'abba'
-//   //   }
-//   // },
-//   // "test something": '123',
-//   // "_id": 123,
-// }
-
-// const user1 = {
-//   name: 'Tomasz'
-// }
-
-// const user2 = {
-//   name: 'Asia'
-// }
-
-// const user3 = {
-//   name: 'Marcin'
-// }
-
-// const users = [user1, user2, user3]
-
-// for (let i = 0; i < users.length; i++) {
-//   console.log(users[i].name)
-// }
-
-// function createObject() {
-
-// }
-
-// const user4 = createObject()
-
-// if (...) {
-
-// }
-
-// const myBook = {
-//   title: 42132,
-//   company: 'lego',
-//   qrcode: "abba",
-//   pages: [
-//     {
-//       id: 1, 
-//       imageUrl: 'someUrl',
-//       numberOfPartsRequired: [1, 2, 17]
-//     }
-//   ],
-//   author: {
-//     name: 'Johny'
-//   }
-// }
-
-// const paramName = 'title'
-
-// const someParam = myBook.title
-
-// myBook.title = '84829'
-// myBook.author.age = 25
-// myBook.author = ''
-// myBook[paramName] = 'abba'
-
-// console.log(myBook.title) // 84.99%
-// console.log(myBook["title"]) // 0.01%
-// console.log(myBook[paramName]) // 15%
-
-// console.log(myBook.author.name)
-
-// const name = 'Wojciech'
-// const lastName = 'Niemczyk'
-// const age = 96
-
-// const user = {
-//   name, // -> name: name
-//   age, // -> age: age
-//   lastName,
-//   // getFullName() {
-//   //   return `${name} - ${lastName}, hello!`
-//   // }
-// }
-
-// function getFullName(name, lastName) {
-//   return `${name} - ${lastName}, hello!`
-// }
-
-// console.log(user)
-// // console.log(user.getFullName())
-// console.log(getFullName(user.name, user.lastName))
-
-// const books = [1, 2, 3, 4]
-
-// // bookShelfList.push({
-// const bookShelf = {
-//   books: ["The Last Kingdom"],
-//   users: ['Mariusz', 'Wojciech'],
-  
-//   getBooks() {
-//     console.log(this.books)
-//   },
-  
-//   addBook(bookName) {
-//     this.books.push(bookName)
-//   },
-// }
-// // )
-
-// // bookShelf.books;
-// bookShelf.getBooks()
-
-// bookShelf.addBook('Harry Potter 1')
-// bookShelf.addBook('Harry Potter 2')
-
-// bookShelf.getBooks()
-
-// const book = {
-//   title: "Harry Potter 3",
-//   author: 'J.K. Rowling',
-//   genres: ['adventure', 'awesome'],
-//   rating: 9.2,
-//   isPublic: true,
+//   rating: 9.2
 // }
 
 // const book2 = {
-//   title: 'Harry Potter 4'
+//   title: 'Harry Potter 6',
+//   author: 'J.K. Rowling',
+//   genres: ['adventure', 'somethingAwesome'],
+//   isPublic: true,
+//   rating: 9.2,
+//   coverImage: 'abba'
 // }
 
-// for (const key in book) {
-//   if (book.hasOwnProperty(key)) {
-//     console.log(key, book[key])
-//   }
-//   // console.log(book[key])
-// }
+// // const accessType = book.isPublic ? "publiczny" : "zamknięty";
+// // const message = `Książka ${book.title} autorstwa ${book.author} z oceną ${book.rating} ma dostęp ${accessType}`
 
-// if (book2.hasOwnProperty('author')) {
-//   console.log(book2.author)
-// } else {
-//   console.log(book.author)
-// }
+// const title = 'This is title for whole page!';
 
-// const keys = Object.keys(book)
-// const values = Object.values(book)
-// const entires = Object.entries(book)
+// const { 
+//   title: firstTitle, 
+//   author: firstAuthor = 'not J.K. Rowling anymore', 
+//   isPublic, 
+//   rating, 
+//   coverImage = 'https://google.com' 
+// } = book;
+// // ->
+// // const title = book.title; -> const firstTitle = book.title
+// // const author = book.author;
+// // const isPublic = book.isPublic;
+// // const rating = book.rating;
+// // const coverImage = book.coverImage || 'https://google.com'
 
-// // for (const key of keys) {
-// //   console.log(key, book[key])
-// // }
+// const accessType = isPublic ? "publiczny" : "zamknięty";
+// const message = 
+//   `Książka ${firstTitle} autorstwa ${firstAuthor} z oceną ${rating} ma dostęp ${accessType}, a ich cover image to: ${coverImage}`
 
-// console.log(keys)
-// console.log(values)
-// console.log(entires)
+// console.log(message)
+// console.log(book.coverImage)
 
-// const goods = {
-//   apples: 6,
-//   bread: 3,
-//   cheese: 7
-// }
+// // console.log(title, author, isPublic, rating)
 
-// const valuesOfGoods = Object.values(goods);
 
-// let total = 0;
+// const books = [
+//   {
+//     title: "The Last Kingdom",
+//     author: "Bernard Cornwell",
+//     rating: 8.38,
+//     stats: {
+//       pages: 123,
+//       buyers: 15,
+//     }
+//   },
+//   {
+//     title: "Beside Still Waters",
+//     author: "Robert Sheckley",
+//     rating: 8.51,
+//     stats: {
+//       pages: 85496,
+//       buyers: 34895
+//     }
+//   },
+// ];
 
-// for (const value of valuesOfGoods) {
-//   total += value;
-// }
-
-// console.log(total)
-
-const books = [
-  {
-    title: "The Last Kingdom",
-    author: "Bernard Cornwell",
-    rating: 8.38,
-  },
-  {
-    title: "Beside Still Waters",
-    author: "Robert Sheckley",
-    rating: 8.51,
-  },
-  {
-    title: "Sen śmiesznego człowieka",
-    author: "Fiodor Dostojewski",
-    rating: 7.75,
-  },
-];
-
-// for (let i = 0; i < books.length; i++) {
-//   const currentBook = books[i]
-//   console.log(currentBook)
-// }
-
+// // for (const { title, author, rating } of books) {
 // for (const book of books) {
-//   console.log(book)
+//   const { 
+//     title, 
+//     author, 
+//     rating, 
+//     stats: { 
+//       pages, 
+//       buyers 
+//     } 
+//   } = book;
+//   // const { pages, buyers } = stats;
+
+//   console.log(title)
+//   console.log(author)
+//   console.log(rating)
+//   console.log(pages)
+//   console.log(buyers)
+
+//   // console.log(book.title)
+//   // console.log(book.author)
+//   // console.log(book.rating)
 // }
 
-// books.forEach((book) => {
-//   console.log(book)
-// })
+// const rgb = [80, 234, 79, 0.98]
+// const [red, green, blue, alfa = 0.3] = rgb;
 
-let totalRating = 0;
+// const [red, green, ...colors] = rgb;
 
-for (const book of books) {
-  totalRating += book.rating
+// console.log(red, green, colors)
+// console.log(red, green, blue, alfa)
+
+// const [, , blue] = rgb
+
+// console.log(blue)
+
+// const rgbObj = { red: 80, green: 234, blue: 79}
+// const {red: redObj, green: greenObj, blue: blueObj} = rgbObj
+
+// console.log(red, green, blue, redObj, greenObj, blueObj)
+
+// function multiply(first, second) {
+//   return first * second
+// }
+
+// multiply(7, 5)
+
+const books = [];
+
+function createBookOld(title, statsBuyers, statsPages, downloads, isPublic, author) {
+  const book = {
+    title, statsBuyers, statsPages, downloads, isPublic, author
+  }
+  books.push(book)
 }
 
-const avgRating = (totalRating / books.length).toFixed(1)
-console.log(avgRating)
+createBookOld('Harry Potter 7', 34534, 1231, 543543, true, 'J.K')
 
-// Object.create()
+// ---------------------
 
-const someObjectMyAwesome = {}
+// function createBookNew(book) {
+//   if (book.title) {
+//     books.push(book)
+//   }
+// }
+
+function createBookNew(book) {
+  const { author: newAuthor, title, stats, isPublic = 'test'} = book;
+  books.push({
+    newAuthor,
+    title,
+    stats,
+    isFinished: isPublic
+  })
+}
+
+// function createBookNew({ author, title, stats, isPublic}) {
+//   // const { author, title, stats, isPublic} = book;
+//   books.push({
+//     author,
+//     title,
+//     stats,
+//     isFinished: isPublic
+//   })
+// }
+
+createBookNew({
+  author: 'J.K',
+  title: 'Harry nie-Potter',
+  stats: {
+    buyers: 12,
+    pages: 543
+  },
+  isPublic: false
+})
+
+console.log(books)
