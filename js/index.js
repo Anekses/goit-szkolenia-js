f74fc080cf3d63710839c95bd44e13c4dedf9144
AdrianRydzynski <adrian.rydzynski@gmail.com>
2023-08-16 21:02:32 +0200

    10 class

--- js/index.js
+++ js/index.js
@@ -1,174 +1,252 @@
-// // this
-
-// const person = {
-//   username: 'Arkadiusz',
-//   showName() {
-//     // console.log(person.username) -> złe!
-//     console.log(this.username)
-//   },
-
-//   books: ["LOTR 1", "LOTR 2"],
-//   getBooks() {
-//     console.log(this)
-//     // return this.books;
-//   },
-//   addBook(bookName) {
-//     this.books.push(bookName)
+// const obj = {
+//   foo() {
+//     console.log(this);
 //   }
 // }
 
-// // person.showName()
-// person.getBooks()
-// // person.addBook('LOTR 3')
-// // console.log(person.getBooks())
+// obj.foo()
 
-// // this.console.log('hello world')
-// console.log(this)
+// const foo = () => console.log(this)
 
-// function calculate() {
-//   console.log(this);
-// }
+// foo()
 
-// const person = {
-//   username: 'Piotr',
-
-//   showCalculate() {
+// const obj1 = {
+//   foo() {
 //     console.log(this)
-//     calculate()
 //   }
 // }
 
-// person.showCalculate()
-// calculate()
-
-
-
-// function showThis() {
-//   console.log(this);
+// function foo() {
+//   console.log(this)
 // }
 
-// let showThis = () => {
-//   console.log(this);
+// const obj2 = {
+//   foo: foo
 // }
 
+// obj1.foo()
+// obj2.foo()
 
-// const person = {
-//   username: 'Piotr',
-
-//   showObject() {
-//     console.log(this)
-//     showThis()
-//   }
+// const obj = {
+//   foo: () => console.log(this)
 // }
 
-// showThis()
-
-// console.log('-----')
-
-// person.showObject()
-
-// console.log('-----')
-
-// person.showAnotherThis = showThis;
-// person.showAnotherThis()
-
-
-// const customer = {
-//   firstName: 'Piotr',
-//   lastName: 'R',
-
-//   getFullName() {
-//     console.log(this)
-//     return `${this.firstName} ${this.lastName}`
-//   }
-// }
-
-// // function getMessage(callback) {
-// //   console.log(this)
-// //   console.log(callback())
-// // }
-
-// const getMessage = (callback) => {
-//   // console.log(this)
-//   console.log(callback())
-// }
-
-// console.log(customer.getFullName())
-// getMessage(customer.getFullName)
+// obj.foo()
 
 // const hotel = {
-//   username: "Resort hotel",
-
+//   username: 'Resort',
 //   showThis() {
-//     const fooArrow = () => {
-//       console.log("this in fooArrow: ", this);
-//     };
+//     const foo = () => console.log(this)
+//     foo()
+//   }
+// }
 
-//     function fooFunction () {
-//       console.log("this in fooFunction: ", this);
-//     };
+// hotel.showThis()
 
-//     fooArrow();
-//     fooFunction();
-//     console.log("this in showThis: ", this);
-//   },
-// };
+// const name = 'Mateusz'
+// const nickname = 'Daria_m'
+// const currency = 'PLN'
+// const iWantToHaveAHouseOnMaledivesToday = true
+// const hotel = 5;
 
-// hotel.showThis();
+// const foo = (arg) => console.log(arg);
 
-function helloWorld(hello, secondOne) {
-  console.log(`${hello}, ${this.username}, ${secondOne}`)
-}
+// const getInfo = (name, nickname, currency, isTrue) => {
+//   console.log(`${name} - ${nickname} - ${currency}`)
+// }
 
-const person1 = {
-  username: 'Mariusz'
-}
+// getInfo(name, nickname, currency)
 
-const person2 = {
-  username: 'Agata'
-}
+// // -----
 
-console.log('CALL')
-helloWorld.call(person1, 'Witaj', 'w domu.')
-helloWorld.call(person2, 'Witaj', 'w domu.')
-helloWorld('Witaj', 'w domu.')
+// const user = {
+//   name: 'Mateusz',
+//   nickname: 'Daria_m',
+//   currency: 'PLN',
+//   hotel: 5,
 
-console.log('APPLY')
-helloWorld.apply(person1, ['Witaj', 'w domu.'])
-helloWorld.apply(person2, ['Witaj', 'w domu.'])
-helloWorld('Witaj', 'w domu.')
+//   getInfo() {
+//     console.log(`${this.name} - ${this.nickname} - ${this.currency}`)
+//   }
+// }
 
-console.log('BIND')
+// // user.getInfo()
 
-const person3 = {
-  username: 'Marcin',
-  firstName: 'Mariusz',
-  lastName: 'Goit'
-}
+// const anotherUser = Object.create(user);
+// anotherUser.bankAmount = 1000;
 
-const heyMarcin = helloWorld.bind(person3)
-const heyAgata = helloWorld.bind(person2)
+// // console.log(user)
+// // console.log(anotherUser)
 
-heyAgata('Cześć', 'na zajeciach')
-heyMarcin('Siema', 'na lekcji')
+// const animal = {
+//   legs: 4,
+// }
 
+// const mammal = Object.create(animal)
+// mammal.isMammal = true;
 
-const customer = {
-  firstName: 'Piotr',
-  lastName: 'R',
+// const dog = Object.create(mammal)
+// dog.woof = () => console.log('woof')
 
-  getFullName() {
-    console.log(this)
-    return `${this.firstName} ${this.lastName}`
+// console.log(dog)
+
+class User {
+  #username = 'Marcin'
+  age = 21
+  isAlive = true
+
+  constructor({ username, age }) {
+    this.#username = username;
+    this.age = age;
+
+    // if (age) {
+    //   this.age = age;
+    // }
+
+    this.age = age || this.age
+  }
+
+  getUsername = () => {
+    return this.#username;
+  }
+
+  getUsernameAndAge = () => {
+    this.#setUsername()
+    return `Hey, tutaj ${this.#username}, mam ${this.age} lat. Witam!`
+  }
+
+  #setUsername = (user) => {
+    // Request na backend do sprawdzenia czy taki user istnieje
+    this.#username = user;
+  }
+
+  sayHello = () => {
+    console.log('Hello')
   }
 }
 
-console.log('---------------------')
+const firstUser = new User({
+  username: 'Mariusz', 
+  age: 32
+})
 
-const getMessage = (callback) => {
-  console.log(callback())
+const secondUser = new User({
+  username: 'Wojciech',
+})
+
+// firstUser.#username = 'Tomasz'
+// firstUser.#setUsername('Arkadiusz')
+
+// console.log(firstUser.getUsernameAndAge())
+// console.log(firstUser.getUsername())
+// console.log(firstUser.#username)
+
+// const user1 = new User();
+// const marcinUser = new User();
+// const marcinUser = {
+//   username: 'Marcin',
+
+//   sayHello() {
+//     console.log('Hello')
+//   }
+// }
+
+// console.log(user1)
+// console.log(marcinUser)
+
+// user1.sayHello()
+// console.log(marcinUser.username)
+
+// class Person {
+//   #email;
+//   static Roles = {
+//     ADMIN: 'admin',
+//     USER: 'user',
+//   }
+//   static #takenEmails = [];
+
+//   static isEmailTaken(email) {
+//     // return `This is ${this.email}, hello to ${email}`
+//     return Person.#takenEmails.includes(email)
+//   }
+
+//   constructor({ email }) {
+//     this.#email = email;
+//     Person.#takenEmails.push(email)
+//   }
+//   get email() {
+//     return this.#email
+//   }
+//   set email(newEmail) {
+//     if (newEmail.indexOf('@') > -1) {
+//       this.#email = newEmail
+//     } else {
+//       console.log('lol nope')
+//     }
+//   }
+// }
+
+// const person = new Person({
+//   email: 'adrian@goit.pl'
+// });
+// console.log(Person.isEmailTaken('adrian@gmail.com'))
+// console.log(Person.isEmailTaken('adrian@goit.pl'))
+
+// person.email = 'abba'
+// person.email = 'abba@gmail.com'
+
+// Person.Roles.USER = 5
+
+// console.log(person.#email)
+// console.log(person)
+// console.log(person.email)
+// console.log(Person.Roles.USER)
+
+// console.log()
+
+class Animal {
+  legs
+  #private1 = 1
+
+  constructor({legs}) {
+    this.legs = legs
+  }
 }
 
-console.log(customer.getFullName())
-getMessage(customer.getFullName.bind(customer))
-getMessage(customer.getFullName.bind(person3))
+class Dog extends Animal {
+  #private2 = 2
+  // woof = () => console.log('woof')
+
+  woof() {
+    console.log('woof')
+  }
+
+  constructor({ legs }) {
+    super( { legs } )
+  }
+}
+
+class Corgi extends Dog {
+  #private3 = 3
+  isAwesome = true;
+  age;
+
+  woof = () => {
+    console.log(('not-woof'))
+    super.woof()
+  }
+
+  constructor({ legs, age }) {
+    super({legs})
+    this.age = age
+    // this.legs = legs
+    this.woof()
+    // console.log(this.#private3)
+  }
+}
+
+const animal = new Animal({ legs: 8 })
+// console.log(animal)
+
+const corgi = new Corgi({ legs: 4, age: 15})
+console.log(corgi)
