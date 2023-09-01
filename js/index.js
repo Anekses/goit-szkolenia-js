8dc740368be87d54d803830dbcb0e2a90460af57
AdrianRydzynski <adrian.rydzynski@gmail.com>
2023-09-01 20:40:05 +0200

    qa

--- js/index.js
+++ js/index.js
@@ -1,118 +1,222 @@
-// const title = document.createElement('h2')
-// title.textContent = 'Hej, GoIT'
-
-// const container = document.querySelector('.container')
-
-// container.append(title)
-// // container.prepend(title)
-// // container.after(title)
-// // container.before(title)
-
-// const button = document.querySelector('.button');
-
-// button.addEventListener(
-    //event -> click, mouseup, keyboarddown, keyboardup
-    //handler -> callback
-    //options -> opcje dla naszego listenera
-// )
-
-// const handleClick = () => {
-//     console.log('button clicked!')
+// Before
+// const hello = (name) => {
+//     return `Hello ${name}`
 // }
 
-// const secondHandleClick = () => {
-//     console.log('button clicked once more!')
-//     button.removeEventListener('click', handleClick)
+// const goodbye = (name, surname) => {
+//     return `Goodbye ${name} - ${surname}`
 // }
 
-// button.addEventListener('click', handleClick)
-// button.addEventListener('click', secondHandleClick)
+// const sayHello = (name, callback) => {
+//     console.log(callback(name))
+// }
 
-// this
+// sayHello('BGal', hello)
+// sayHello('Asia', goodbye)
 
-// const mango = {
-//     username: "mango",
-//     showUsername() {
-//         console.log(this)
-//         console.log(`My username is ${this.username}`)
+//After
+// const hello = (name) => {
+//     return `Hello ${name}`
+// }
+
+// const goodbye = (name, surname) => {
+//     return `Goodbye ${name} - ${surname}`
+// }
+
+// const sayHello = (isHello, name, surname) => {
+//     if (isHello) {
+//         const result = hello(name);
+//         console.log(result)
+//     } else {
+//         const result = goodbye(name, surname)
+//         console.log(result)
 //     }
 // }
 
-// mango.showUsername();
+// sayHello(true, 'BGal')
+// sayHello(false, 'Asia', 'Dobro')
 
-// button.addEventListener("click", mango.showUsername.bind(mango))
+// const select = document.querySelector('.pizza-select');
+// const select2 = document.querySelector('.pizza-select2');
+// const select3 = document.querySelector('.pizza-select3');
 
-// const button = document.querySelector('.button');
-
-// const handleClick = (event) => {
-//     console.log('clicked!')
-//     console.log(event.currentTarget)
-// }
-
-// // e, ev, evt, event
-// // button.addEventListener('click', (event) => handleClick(event))
-// button.addEventListener('click', handleClick)
-// button.addEventListener('mousedown', handleClick)
-
-// const form = document.querySelector('.register-form')
-
-// form.addEventListener('submit', (event) => {
-//     event.preventDefault()
-//     // console.log(event.currentTarget.elements)
-//     const {
-//         elements: { username, email }
-//     } = event.currentTarget;
-
-//     console.log(username.value, email.value)
+// // First
+// select.addEventListener('change', (event) => {
+//     console.log(event.target)
+// })
+// select2.addEventListener('change', (event) => {
+//     console.log(event.target)
+// })
+// select3.addEventListener('change', (event) => {
+//     console.log(event.target)
+//     handleChange(event, 15, 'abbas')
 // })
 
-// const handleClick = (event) => {
-//     console.log({event})
+// // Second
+// const handleChange = (event) => {
+//     console.log(event.target)
 // }
 
-// document.addEventListener('keydown', handleClick) // <- it's good!
+// select.addEventListener('change', handleChange)
+// select2.addEventListener('change', handleChange)
+// select3.addEventListener('change', handleChange)
 
-// document.addEventListener('keyup', handleClick)
+// const sum = (array) => {
+//     let sum = 0;
 
-// document.addEventListener('keydown', event => {
-//     event.preventDefault()
-
-//     console.log(event.code)
-
-//     //event.keyCode
-
-//     if(event.ctrlKey && event.code === 'KeyP') {
-//         console.log('print!')
+//     for (let i = 0; i < array.length; i++) {
+//         const current = array[i];
+//         sum += current;
 //     }
+
+//     return sum;
+// }
+
+// const array = [1, 3, 5, 7, 9, 100];
+
+// console.time('Function 1')
+// const result = sum(array);
+// console.timeEnd('Function 1')
+
+// console.time('Function 2')
+// const reduceResult = array.reduce((acc, number) => {
+//     return acc + number;
+// }, {})
+// console.timeEnd('Function 2')
+
+// console.log(result)
+// console.log(reduceResult)
+
+// const cars = ['BMW', 'Hyundai', 'Skoda', 'BMW', 'BMW', 'VW', 'Audi', 'Audi', 'Skoda'];
+
+// const carsSorted = cars.reduce((acc, curr) => {
+//   // if (acc[curr]) ...
+//   if (curr in acc) {
+//     acc[curr]++
+//   } else {
+//     acc[curr] = 1
+//   }
+
+//   return acc;
+// }, {})
+
+// console.log(carsSorted)
+
+
+// .map -> nową tablicę
+// .forEach -> działać na obecnej
+
+// const textOutput = document.querySelector('.text-output')
+// const html = `<span>Hello</span>`
+
+// textOutput.innerHTML += html;
+// textOutput.append(html)
+
+// console.log(textOutput)
+
+// 1
+  // this wskazuje na obiekt/klasę -> JEST OK
+// 2
+  // this wskazuje na coś innego -> use .bind
+
+// for / do-while / while
+  // while -> 0.1%
+  // do-while -> 1.9%
+  // for -> 98%
+
+// let weight = 0;
+
+// for (let i = 0; weight <= 10; i++) {
+//   weight++;
+// }
+
+// do {
+//   zrób zapytanie do backendu, jeśli backend zwróci "tutaj masz 100"
+//   "jak chcesz reszte to strona 2"
+//   -> pytamy raz jeszcze o strone 2
+// } while()
+
+// const functionMyAwesome = () => {}
+
+// const cat = {
+//   lives: 9,
+//   // jumps: () => {}
+//   jumps() {
+//     // some() {}
+//     some: () => {}
+//   }
+// }
+
+// const button = document.querySelector('.goit')
+
+// const handler = () => {
+//   // console.log(this)
+//   // console.log('------------')
+//   userObj.showUser()
+//   userObj.showUserFunc()
+// }
+
+// function handlerFunc() {
+//   // console.log(this);
+//   userObj.showUser()
+//   userObj.showUserFunc()
+// }
+
+// const userObj = {
+//   name: 'Asia',
+//   showUserFunc() {
+//     console.log(`Func: ${this.name}`)
+//   },
+//   showUser: () => {
+//     console.log(`Arrow: ${this.name}`)
+//   }
+// }
+
+// const array = [1, 2, 3, 4, 5]
+
+// array.map((number) => {
+
 // })
 
-const textOutput = document.querySelector('.text-output')
-const valueOutput = document.querySelector('.value-output')
-const select = document.querySelector('.pizza-select')
+// button.addEventListener('click', handler)
 
-select.addEventListener('change', event => {
-    const currentTarget = event.currentTarget;
-    const { value, selectedIndex, options } = currentTarget;
-    const text = options[selectedIndex].text
+// button.addEventListener('click', handlerFunc)
 
-    console.log({value, selectedIndex, options})
+// const user = {
+//   username: 'Asia',
+//   showName() {
 
-    valueOutput.textContent = value;
-    textOutput.textContent = text;
+//   } 
+// }
+
+const button = document.querySelector('.goit')
+
+button.addEventListener('click', (event) => {
+  console.log({
+    currentTarget: event.currentTarget,
+    value: event.currentTarget.value
+  })
+});
+
+const registerForm = document.querySelector('.register-form');
+
+registerForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  console.log({
+    currentTarget: event.currentTarget,
+    value: event.currentTarget.value,
+    elements: event.currentTarget.elements,
+    username: event.currentTarget.elements[0].value,
+    target: event.target
+  })
 })
 
 const textInput = document.querySelector('.text-input')
-const textInputOutput = document.querySelector('.output');
 
-textInput.addEventListener('input', event => {
-    const value = event.currentTarget.value;
-
-    if (value.length < 8) {
-        textInputOutput.textContent = "minimum 8 znaków!"
-    } else {
-        textInputOutput.textContent = value;
-    }
-    
+textInput.addEventListener('input', (event) => {
+  console.log({
+    currentTarget: event.currentTarget,
+    value: event.currentTarget.value
+  })
 })
-
-//document.activeElement
