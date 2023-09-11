d15277ccd73694cd5a92d0d26149ade1bb8bc86e
AdrianRydzynski <adrian.rydzynski@gmail.com>
2023-09-11 20:51:36 +0200

    15 class

---
+++ node-tut/greeter.js
@@ -0,0 +1,13 @@
+const helloMessage = 'hello';
+const goodbyeMessage = 'goodbye';
+
+// export const hello = () => helloMessage
+// export const goodbye = () => goodbyeMessage
+
+const hello = () => helloMessage
+const goodbye = () => goodbyeMessage
+
+export {
+    hello,
+    goodbye
+}
