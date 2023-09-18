c7c390c3cced2807039cb1fbf684a7391d18cb9a
AdrianRydzynski <adrian.rydzynski@gmail.com>
2023-09-18 20:42:52 +0200

    17 class

--- js/index.js
+++ js/index.js
@@ -1,47 +1,103 @@
-// const abba = 'Hello'
+// {
+//     username: "Mango",
+//     age: 2
+// }
 
-// // console.log(localStorage.getItem('color'))
-// // console.log(localStorage.setItem('color', 'blue'))
-// // console.log(localStorage.getItem('color'))
+// {
+//     "username": "Mango",
+//     "age": 2
+// }
 
-// // sessionStorage.setItem('color', 'red')
-// // sessionStorage.getItem('color')
+// const data = JSON.parse("Welcome to the future!")
 
+// // ----
 // const user = {
-//     color: 'green',
-//     tea: true,
-//     numbers: [1, 2, 3, 5]
+//     username: "Mango",
+//     showName() {
+//         console.log(this.username)
+//     }
 // }
 
-// try {
-//     localStorage.setItem("user", JSON.stringify(user))
-//     JSON.parse(localStorage.getItem('user'))
-// } catch (error) {
-//     console.log({ error })
+// const json = JSON.stringify(user)
+// console.log(json)
+
+// console.log('Before')
+
+// setTimeout(() => {
+//     console.log('Inside - 3000')
+// }, 3000)
+
+
+// setInterval(() => {
+//     console.log('Interval!')
+// }, 2000)
+
+
+
+// console.log('After')
+
+
+// const callback = (text) => {
+//     console.log(text)
+//     console.log('Inside - 1000')
 // }
 
-// localStorage.setItem('color', 'blue')
-// console.log(localStorage.getItem('color'))
+// setTimeout(callback, 1000, 'some text', )
 
-// // localStorage.removeItem('color')
-// // console.log(localStorage.getItem('color'))
 
-// localStorage.clear()
+// setTimeout(() => {
+//     console.log('2')
+// }, 2)
 
-// import "simplelightbox/dist/simple-lightbox.min.css";
+// setTimeout(() => {
+//     console.log('1')
+// }, 1)
 
-const form = document.querySelector('.form')
-const lsKey = 'messageInput';
 
-form.elements.message.value = localStorage.getItem(lsKey) ?? ""
 
-form.addEventListener('input', event => {
-    localStorage.setItem(lsKey, event.target.value)
-})
+// const timerId = setTimeout(() => {
+//     console.log('Inside - 3000')
+// }, 3000)
 
-form.addEventListener('submit', event => {
-    event.preventDefault()
-    localStorage.removeItem(lsKey)
-    // zamiast usuwac z LS, usun z lsKey, ale dodaj do tablicy
-    form.reset()
-})
+// console.log(timerId)
+
+// clearTimeout(timerId)
+
+
+// const date = new Date(0);
+// const date2 = new Date(60000);
+// const date3 = new Date();
+
+// const date = new Date("March 16, 2030")
+// const date2 = new Date("March 16, 2030 14:24:55")
+
+// console.log(date)
+// console.log(date2)
+
+// // const date3 = new Date(2030, 2, 16, 14, 25, 3, 0)
+// const date3 = new Date(2030, 2, 16)
+// console.log(date3)
+
+// const date = new Date()
+
+// console.log(date.toString())
+// console.log(date.toTimeString())
+// console.log(date.toLocaleTimeString())
+// console.log(date.toUTCString())
+// console.log(date.toDateString())
+// console.log(date.toISOString())
+// console.log(date.toLocaleString())
+// console.log(date.getTime())
+
+const date = new Date()
+
+// setInterval(() => {
+//     console.timeEnd('start')
+//     // console.clear()
+
+//     // date.setTime(date.getTime() + 1000)
+//     // console.log(date.toLocaleString())
+
+//     console.log(new Date().toLocaleString())   
+//     console.time('start')
+// }, 1000)
